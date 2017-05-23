2eb2a06346af14fdeaa04774f6cfde4bdd850ad6
Makgora <titouan.parand@outlook.fr>
2017-05-23 19:26:12 +0200

    switchMenue completed

--- commands.py
+++ commands.py
@@ -1,6 +1,29 @@
+#to stop taskmaster manually
 def quit():
 	print(" -- End ! -- ")
 	exit(0);
 
+#status of all the programs described in the config file, or a specific program, or a specific instance of a program
+def status(program=None, pid=None):
+	print("TODO");
+
+#start a new instance of a program
+def start(program):
+	print("TODO");
+
+#stop a program, or a specific instance of a program
+def stop(program, pid=None):
+	print("TODO");
+
+#stop a program or a specific instance of a program, then restart a new instance of himself
+def restart(program, pid=None):
+	stop(program, pid)
+	start(program);
+
+#reload the configuration file
+def reload():
+	print("TODO");
+
+#If the written command is unknown
 def default():
 	print("Unknown command");
--- taskmaster.py
+++ taskmaster.py
@@ -2,10 +2,29 @@
 import commands
 
 def switchMenu(choice):
+
 	if(choice == "exit"):
 		commands.quit();
+	
+	elif(choice == "status"):
+		commands.status();
+	
+	elif(choice == "start"):
+		commands.start();
+
+	elif(choice == "stop"):
+	commands.stop();
+
+	elif(choice == "restart"):
+	commands.restart();
+
+	elif(choice == "reload"):
+		commands.reload();
+
 	else:
 		commands.default();
+
+##########################################################################
 
 print(" -- Start ! -- ")
 
