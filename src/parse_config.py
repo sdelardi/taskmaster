2ed683103c80152ef7fad2bdb21806b76961d83c
Steeve DELARDIERE <sdelardi@e1r4p17.42.fr>
2017-05-24 12:24:13 +0200

    Parsing OK

--- src/parse_config.py
+++ src/parse_config.py
@@ -1,15 +1,23 @@
 import yaml
+import program
 
+#Parsing du fichier de config
 def parseConfig(file):
-	toto = yaml.load(file)
-	print(toto)
+	datas = yaml.load(file)
+	programs = []
+	for (key, pro) in datas.items():
+		programs.append(program.Program(pro, key))
+	return programs
 
-def openConfig(file_name):
+#Opening config file and parse it / Exit in case of file not found
+def parsing(file_name):
 	try:
 		file = open(file_name, "r")
 	except FileNotFoundError:
-		print("File not found")
+		print("Config file not found")
 		exit(2)
-	parseConfig(file)
+	programs = parseConfig(file)
+	file.close()
+	return programs
 
-openConfig("config.conf")
+parsing("config.conf")
