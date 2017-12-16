ae4294beb3f4ff0fb8a162d605e18394f14a3fe4
tparand <tparand@student.42.fr>
2017-12-16 20:51:56 +0100

    main adjust

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 func main() {
 	var programs []program;
-	parse_config("./config.xml", &programs);
 	var program_database map[string]program;
 	var process_database map[string][]process;
+
+	parse_config("./config.xml", &programs);
 	init_database(programs, &program_database, &process_database);
 }
