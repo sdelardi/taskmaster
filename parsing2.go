068cb1f71aba24706bfe2ac933e1a3aaae3d4965
tparand <tparand@student.42.fr>
2017-12-16 16:56:23 +0100

    add fill_program_array func

--- parsing2.go
+++ parsing2.go
@@ -1,25 +1,5 @@
 package main
 
-import "encoding/xml";
-import "io/ioutil";
-import "os";
-import "fmt";
-
-func check(e error) {
-	if e != nil {
-		fmt.Printf("error: %v\n", e);
-		os.Exit(1);
-	}
-}
-
-func parse_config(path string) {
-	v := Elements{};
-	dat, err := ioutil.ReadFile(path);
-	check(err);
-	e := xml.Unmarshal([]byte(dat), &v);
-	check(e);
-}
-
 func check_name(Name string) string {
 	//TODO
 	return "";
